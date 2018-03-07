3cd93a95220bab2ef35022dd234b16d13d6d2315
ww954950195 <954950195@qq.com>
2018-03-07 12:03:45 +0800

    Update pymssql连接.py

--- 数据库/pymssql连接.py
+++ 数据库/pymssql连接.py
@@ -5,7 +5,7 @@ import time
 import sys
 
 #数据库服务器信息
-conn  = pymssql.connect(server="192.168.1.193", user="sa", password="ren@#3489", database="ren",charset="ISO-8859-1")  #用此语句连接，获得连接对象。charset="ISO-8859-1"根据每台电脑实际设置
+conn  = pymssql.connect(server="192.168.1.193", user="sa", password="*****", database="ren",charset="ISO-8859-1")  #用此语句连接，获得连接对象。charset="ISO-8859-1"根据每台电脑实际设置
 
 cursor = conn.cursor()  # %获得游标。
 
